afd55babf3a1d2047462f716a2e0b9cf3e848edd
akhoipham <anhkhoipham03@gmail.com>
2026-06-13 21:07:36 -0300

    Debug print

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ import time
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parent))
+print("Hello World")
 
 from intel import dashboard, store
 from intel.ingest import Article
